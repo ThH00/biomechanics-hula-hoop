e3fc85852a802d368373d44db62f7c6c95297875
Chrystal Chern <52893467+chrystalchern@users.noreply.github.com>
2025-07-04 13:55:20 -0700

    experiment - variable names, angles

--- experiments/process_movella.py
+++ experiments/process_movella.py
@@ -8,18 +8,21 @@ def load_movella(file,
     # Get the column labels
     with open(file, "r") as readfile:
         header_keys = readfile.readlines()[header_row-1].split(',')
-    # Identify the column indices for each acceleration component
+    # Identify the column indices for each acceleration and Euler angle component
     time_index = header_keys.index('SampleTimeFine')
-    x_index = header_keys.index('Acc_X')
-    y_index = header_keys.index('Acc_Y')
-    z_index = header_keys.index('Acc_Z')
+    x_accel = header_keys.index('Acc_X')
+    y_accel = header_keys.index('Acc_Y')
+    z_accel = header_keys.index('Acc_Z')
     x_angle = header_keys.index('Euler_X')
     y_angle = header_keys.index('Euler_Y')
     z_angle = header_keys.index('Euler_Z')
     data = np.loadtxt(file,
                 delimiter=",",
                 skiprows=header_row, # Get all the rows after the header
-                usecols=[time_index,x_index,y_index,z_index] # Get only the columns with acceleration data
+                usecols=[time_index,                # time_column
+                         x_accel,y_accel,z_accel,   # acceleration column (m/s)
+                         x_angle,y_angle,z_angle    # Euler angles column (deg)
+                         ]
                 )
     # Change the units of time to seconds and start at zero
     data[:,0] = (data[:,0]-data[0,0])/1000000 
@@ -42,5 +45,5 @@ def detrend(time,
     if backend=='scipy':
         return signal.detrend(data,type=fit)
     elif backend=='polynomial':
-        pmial = Polynomial.fit(time,data,deg=degree)
-        return data - pmial(time)
+        pnom = Polynomial.fit(time,data,deg=degree)
+        return data - pnom(time)
