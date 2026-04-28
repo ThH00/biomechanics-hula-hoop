59980f21e79769bffb7fe51c6a09643c8ed10551
Chrystal Chern <52893467+chrystalchern@users.noreply.github.com>
2026-04-28 09:00:03 -0700

    Fix typo in paper reference comment

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 """ 
 Created by Theresa E. Honein and Chrystal Chern as part of the
-accompanying code and data for the 2026 paer, "The Biomechanics of Hula Hooping"
+accompanying code and data for the 2026 paper, "The Biomechanics of Hula Hooping"
 by C. Chern, T. E. Honein, and O. M. O'Reilly.
 
 Licensed under the GPLv3. See LICENSE in the project root for license information.
